80b39747f3533d147fa4d3d5826ec53b46795773
Luca <lovhy54@outlook.it>
2024-07-15 12:22:11 +0200

    swagger

---
+++ SERVER/swagger.js
@@ -0,0 +1,15 @@
+import swaggerAutogen from 'swagger-autogen';
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Description'
+  },
+  host: 'localhost:3000'
+};
+
+const outputFile = './swagger-output.json';
+const routes = ['./server.js'];
+
+
+swaggerAutogen()(outputFile, routes, doc);
